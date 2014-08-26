3b4ed3fadbc5e79690cd7d24f5ec5c032b9789aa
daisuke <banana0928@dmba.local>
2014-08-26 13:46:48 +0900

    勉強したのでコメントつけて理解を深めた

--- kamo.js/app/js/autocomplete.kamo.js
+++ kamo.js/app/js/autocomplete.kamo.js
@@ -1,38 +1,56 @@
 jQuery(function ($) {
-  kamo.Stream.fromEventHandlerFunction(
-    $('#input'),
-    'keyup'
-  ).debounce(1000).map(function (event) {
-    return event.target.value;
-  }).filter(function (message) {
-    return message.length >= 3;
-  }).scan([], function (result, message) {
-    return [result[1], message];
-  }).filter(function (message) {
-    return message[0] != message[1];
-  }).map(function (message) {
-    return message[1];
-  }).flatMapLatest(function (message) {
-    return kamo.Stream.fromEventHandlerFunction(
-      $.ajax({
-        url: 'http://en.wikipedia.org/w/api.php',
-        dataType: 'jsonp',
-        data: {
-          action: 'opensearch',
-          format: 'json',
-          search: window.encodeURI(message)
-        }
-      }),
-      'done'
+
+  // input[type=text]へkeyupイベントのセット
+  kamo.Stream.fromEventHandlerFunction($('#input'), 'keyup')
+
+    // keyup後1000秒間keyupがなければmapへ
+    .debounce(1000)
+    // eventストリームをフォームのvalueストリームへ
+    .map(function (event) {
+      return event.target.value;
+    })
+    // valueが3文字未満で終了
+    .filter(function (message) {
+      return message.length >= 3;
+    })
+    // 直近の入力と最新の入力の配列ストリームへ変換
+    .scan([], function (result, message) {
+      return [result[1], message];
+    })
+    // 直近の入力と最新の入力が同じであれば終了
+    .filter(function (message) {
+      return message[0] != message[1];
+    })
+    // 最新の入力valueストリームへ変換
+    .map(function (message) {
+      return message[1];
+    })
+    // 新しいストリームへ変換
+    // wikipediaのapiを叩いて、候補を取得する
+    // $.ajaxのdoneが呼ばれるとpublishされる
+    .flatMapLatest(function (message) {
+      return kamo.Stream.fromEventHandlerFunction(
+        $.ajax({
+          url: 'http://en.wikipedia.org/w/api.php',
+          dataType: 'jsonp',
+          data: {
+            action: 'opensearch',
+            format: 'json',
+            search: window.encodeURI(message)
+          }
+        }),
+        'done'
+      );
+    })
+    // 候補を受け取るとviewへ反映させて終了
+    .subscribe(
+      (function ($candidates) {
+        return function (data) {
+          $candidates.empty();
+          $.each(data[1], function (_, message) {
+            $('<li>').append(document.createTextNode(message)).appendTo($candidates);
+          });
+        };
+      })($('#candidates'))
     );
-  }).subscribe(
-    (function ($candidates) {
-      return function (data) {
-        $candidates.empty();
-        $.each(data[1], function (_, message) {
-          $('<li>').append(document.createTextNode(message)).appendTo($candidates);
-        });
-      };
-    })($('#candidates'))
-  );
 });
